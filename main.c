93c50cd78e4b347e3535eac7bfaddb3664fc9fc9
kandirou0 <simo.kandirou15@gmail.com>
2022-09-12 17:32:10 +0100

    update

--- main.c
+++ main.c
@@ -101,12 +101,12 @@ int main()
     printf("le nbr de occurance est : %d",occu);*/
 
 
-    float t[10],s=0;
+    /*float t[10],s=0;
     float m;
     int sup=0;
     printf("saisir les notes : \n");
     for(int j=0;j<10;j++){
-        scanf("%d",&t[j]);
+        scanf("%f",&t[j]);
     }
     for(int i=0;i<10;i++){
         s+=t[i];
@@ -117,8 +117,101 @@ int main()
             sup++;
         }
     }
-    printf("la moyeene est : %f\n",m);
-    printf("nombre des notes superieur est : %d\n",sup);
+    printf("la moyeene est : %f \n",m);
+    printf("nombre des notes superieur est : %d\n",sup);*/
+
+
+    /*float t[20],s;
+    s=1000;
+    for(int j=0;j<20;j++){
+        s=s+s*0.02;
+        t[j]=s;
+    }
+    for(int k=0;k<20;k++){
+        printf("le montant d'anne %d est : %f \n",k,t[k]);
+    }
+
+    */
+
+    /*int t[6];
+    int t2[6];
+    printf("saisir le tableau : \n");
+    for(int j=0;j<6;j++){
+        scanf("%d",&t[j]);
+    }
+    for(int i=0;i<6;i++){
+        t2[i]=t[5-i];
+    }
+    printf("le tableau reverse: \n");
+    for(int j=0;j<6;j++){
+        printf("%d \n",t2[j]);
+    }*/
+
+
+    /*int n;
+    int nbr=2;
+    int count=0;
+    int premier=0;
+    printf("saisir un nombre : \n");
+    scanf("%d",&n);
+    int t[n];
+    do{
+        for(int i=2;i<nbr;i++){
+            if(nbr%i==0){
+                premier=1;
+                break;
+            }
+        }
+        if(premier==0){
+            t[count]=nbr;
+            count++;
+        }
+        nbr++;
+        premier=0;
+    }while(count<=n);
+
+    for(int j=0;j<n;j++){
+        printf("les nombres premier sont : %d\n",t[j]);
+    }*/
+
+    /*int t[10];
+    int max1,max2;
+    printf("les valeurs :\n");
+    for(int j=0;j<10;j++){
+        scanf("%d",&t[j]);
+    }
+    max1=t[0];
+    max2=t[0];
+    for(int i=0;i<10;i++){
+        if(max1<t[i]){
+            max2=max1;
+            max1=t[i];
+        }
+        if(max2<t[i] && max1>t[i]){
+            max2=t[i];
+        }
+    }
+    printf("Max 1 : %d\n",max1);
+    printf("Max 2 : %d\n",max2);*/
+
+
+    int t[10]={3,7,9,10,54};
+
+    if(){}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
